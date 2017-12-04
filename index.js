8cd22cdcb0f2d37fc389cfa138550926b4825f2a
Artthik Sellathurai <artthik.sellathurai@students.unibe.ch>
2017-12-04 13:48:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   let a = facts.length;
   while(a != 0){
-    console.log(facts[0]+"!!!");
+    facts[0] = facts[0]+"!!!");
     a--;
   }
   return facts;
